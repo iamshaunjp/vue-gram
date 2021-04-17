bdf16c4ad976e3e890862dd3d01722eecf16b807
Shaun <shaun@thenetninja.co.uk>
2021-04-17 15:35:24 +0100

    lesson-3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="App">
+    <Title />
     <ImageGrid />
   </div>
 </template>
 
 <script>
 import ImageGrid from './components/ImageGrid.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    ImageGrid
+    ImageGrid,
+    Title
   }
 }
 </script>
---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <div className="title">
+    <h1>VueGram</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </div>
+</template>
+
+<style>
+  .title h1{
+    color: var(--secondary);
+    font-size: 2rem;
+    letter-spacing: 2px;
+    margin: 40px 0 20px 0;
+    text-align: center;
+  }
+  .title p{
+    text-align: center;
+    color: #aaa;
+  }
+</style>
