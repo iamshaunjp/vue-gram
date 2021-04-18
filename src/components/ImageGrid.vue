c4d21eb2eb888a4d2d0aa6e4680bb16f9545d7b7
Shaun <shaun@thenetninja.co.uk>
2021-04-18 13:39:36 +0100

    lesson-13

--- src/components/ImageGrid.vue
+++ src/components/ImageGrid.vue
@@ -1,5 +1,13 @@
 <template>
-  image grid goes here
+  <div class="img-grid">
+    <div 
+      v-for="img in documents" 
+      :key="img.id" 
+      class="img-wrap"
+    >
+      <img :src="img.url" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,5 +23,26 @@ export default {
 </script>
 
 <style>
-
+  .img-grid{
+    margin: 20px auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 40px;
+  }
+  .img-wrap{
+    overflow: hidden;
+    height: 0;
+    padding: 50% 0;
+    /* padding controls height, will always be perfectly square regardless of width */
+    position: relative;
+    opacity: 0.8;
+  }
+  .img-wrap img{
+    min-width: 100%;
+    min-height: 100%;
+    max-width: 180%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 </style>
